d900c99b90d02bc28a25981ba52e89047704d02e
aleksim1 <aleksi.moilanen02@gmail.com>
2026-04-29 17:34:54 +0300

    updated backend root url

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 //const BACKEND_ROOT_URL = 'http://localhost:3001'
-const BACKEND_ROOT_URL = 'https://todo-backend-xxzl.onrender.com'
+const BACKEND_ROOT_URL = 'https://wpp-todo-back.onrender.com'
 import { Todos } from "./class/Todos.js"
 
 const todos = new Todos(BACKEND_ROOT_URL)
